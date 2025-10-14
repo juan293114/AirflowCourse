80f4c44f82cb22aa3bd13f7e268b30690b6d182d
jdravila <jdravila@bancolombia.com.co>
2025-10-13 20:52:28 -0500

    Agregar copia a bronce

--- Laboratorios/1_airflow-intro-lab-uno/dags/elt_medallon.py
+++ Laboratorios/1_airflow-intro-lab-uno/dags/elt_medallon.py
@@ -10,11 +10,18 @@ if str(PROJECT_ROOT) not in sys.path:
     sys.path.append(str(PROJECT_ROOT))
 
 from elt.ingest_raw import ingest_to_raw
+from elt.bronze import copy_raw_to_bronze
 
 BOGOTA_TZ = pendulum.timezone("America/Bogota")
 
+
 DATA_LAKE_ROOT = Path("/opt/airflow/data_lake")
+
+#Capa raw
 RAW_ROOT = DATA_LAKE_ROOT / "raw" / "tvmaze"
+
+#Capa bronze
+BRONZE_PATH = DATA_LAKE_ROOT / "bronze" / "tvmaze"
 
 INGEST_PARAMS = {
     "start_date": pendulum.date(2020, 1, 1),
@@ -23,7 +30,14 @@ INGEST_PARAMS = {
     "timeout": 30,
 }
 
+BRONZE_PARAMS = { 
+    "raw_root": str(RAW_ROOT),
+    "bronze_path": str(BRONZE_PATH / "tvmaze.parquet"),
+}
+
+
 with DAG(
+
     dag_id="elt_medallon",
     schedule="0 5 * * *",
     start_date=pendulum.datetime(2025, 10, 10, tz=BOGOTA_TZ),
@@ -35,5 +49,11 @@ with DAG(
         python_callable=ingest_to_raw,
         op_kwargs=INGEST_PARAMS,
     )
+
+    bronze_task = PythonOperator(
+        task_id="copy_to_bronze",
+        python_callable=copy_raw_to_bronze,
+        op_kwargs=BRONZE_PARAMS,
+    )
     
-    ingest_task 
+    ingest_task >> bronze_task 
