f8cc8a1c908ecff4c6160cb72f0225aef0d6fcfe
jdravila <jdravila@bancolombia.com.co>
2025-10-19 20:54:55 -0500

    Ajustes demo

--- Laboratorios/3_airflow-sensor-lab-uno/dags/trigger_dagrun_demo.py
+++ Laboratorios/3_airflow-sensor-lab-uno/dags/trigger_dagrun_demo.py
@@ -1,5 +1,3 @@
-"""Demo: one DAG launches another using TriggerDagRunOperator."""
-
 import pendulum
 from airflow.models import DAG
 from airflow.operators.empty import EmptyOperator
@@ -8,19 +6,16 @@ from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 BOGOTA_TZ = pendulum.timezone("America/Bogota")
 DEFAULT_ARGS = {"start_date": pendulum.datetime(2025, 10, 1, tz=BOGOTA_TZ)}
 
-CHILD_DAG_ID = "trigger_target_dag"
-PARENT_DAG_ID = "trigger_launcher_dag"
-
 # Child DAG: the DAG that will be triggered externally.
-child_dag = DAG(
-    dag_id=CHILD_DAG_ID,
+trigger_target_dag = DAG(
+    dag_id="trigger_target_dag",
     schedule=None,
     default_args=DEFAULT_ARGS,
     catchup=False,
     tags=["demo", "trigger"],
 )
 
-with child_dag:
+with trigger_target_dag:
     child_start = EmptyOperator(task_id="child_start")
     child_task = EmptyOperator(task_id="child_task")
     child_end = EmptyOperator(task_id="child_end")
@@ -28,20 +23,20 @@ with child_dag:
     child_start >> child_task >> child_end
 
 # Parent DAG: triggers the child DAG when it runs.
-parent_dag = DAG(
-    dag_id=PARENT_DAG_ID,
+trigger_launcher_dag = DAG(
+    dag_id="trigger_launcher_dag",
     schedule="@daily",
     default_args=DEFAULT_ARGS,
     catchup=False,
     tags=["demo", "trigger"],
 )
 
-with parent_dag:
+with trigger_launcher_dag:
     start = EmptyOperator(task_id="start")
 
     trigger_child = TriggerDagRunOperator(
         task_id="trigger_child",
-        trigger_dag_id=CHILD_DAG_ID,
+        trigger_dag_id="trigger_target_dag",
         wait_for_completion=True,
         poke_interval=10,
     )
@@ -49,7 +44,3 @@ with parent_dag:
     finish = EmptyOperator(task_id="finish")
 
     start >> trigger_child >> finish
-
-
-globals()[CHILD_DAG_ID] = child_dag
-globals()[PARENT_DAG_ID] = parent_dag
